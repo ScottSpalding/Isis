130772c1c491928495172e1984f4a6f6e2543254
Ricter <ricter@gmail.com>
2018-02-12 19:39:43 -0800

    Initial setup of Unreal project.

---
+++ Source/Isis/IsisGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "IsisGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ISIS_API AIsisGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
